15eefe7c95acae5a7ed43eebd028e6ce0accb9e0
Ashok Sasitharan <asasitharan28@gmail.com>
2020-12-17 15:00:04 -0500

    added BMI image

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -2,8 +2,13 @@ package ca.durhamcollege;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 public class Main extends Application
 {
@@ -13,9 +18,20 @@ public class Main extends Application
     {
         primaryStage.setTitle("Java Lab 5");
 
-        GridPane gridPane = new GridPane();
+        //set up the path
+        FileInputStream imagePath = new FileInputStream("src/ca/durhamcollege/bmiTable.PNG");
+        //make the image
+        Image bmiTable = new Image(imagePath);
+        //make the imageview
+        ImageView bmiTableView = new ImageView(bmiTable);
+        bmiTableView.setFitWidth(200);
+        bmiTableView.setPreserveRatio(true);
 
-        Scene scene = new Scene(gridPane, 320, 480);
+        HBox hbox =new HBox(10,bmiTableView);
+
+        GridPane gridPane = new GridPane();
+        gridPane.add(hbox,0,1);
+        Scene scene = new Scene(gridPane, 640, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
