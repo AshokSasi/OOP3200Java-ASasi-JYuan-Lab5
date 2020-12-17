a0870a39d6a029b2ba28ab86ab5fd65dcbdb0e66
Ashok Sasitharan <asasitharan28@gmail.com>
2020-12-17 16:10:26 -0500

    added comments

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,3 +1,9 @@
+/*
+Name: Ashok Sasitharan 100745484, Jacky Yuan 100520106
+Date: December 17 2020
+OOP3200 Java Lab 5
+Changes:N/A
+ */
 package ca.durhamcollege;
 
 import javafx.application.Application;
@@ -25,9 +31,11 @@ public class Main extends Application
     {
         primaryStage.setTitle("Java Lab 5");
 
+        // CONTROLS
         Label MyHeight = new Label ("My Height(Inches):");
         Label MyWeight = new Label ("My Weight(Pounds):");
         Label lblBMI = new Label ("BMI: ");
+        Label errorLbl = new Label ("");
         Label outputBMI = new Label ("");
         TextField txtHeight = new TextField();
         TextField txtWeight = new TextField();
@@ -39,9 +47,10 @@ public class Main extends Application
         Image bmiTable = new Image(imagePath);
         //make the imageview
         ImageView bmiTableView = new ImageView(bmiTable);
-        bmiTableView.setFitWidth(200);
+        bmiTableView.setFitWidth(250);
         bmiTableView.setPreserveRatio(true);
 
+        //button click event that calculates BMI
         class ButtonClickerHandler implements EventHandler<ActionEvent>
         {
             @Override
@@ -50,15 +59,26 @@ public class Main extends Application
                 double weight=0.0;
                 double height=0.0;
                 double BMI=0.0;
-                try {
-                    weight = Integer.parseInt(txtWeight.getText());
-                    height = Integer.parseInt(txtHeight.getText());
-                    BMI = (weight*703)/(height*height);
+                try
+                {
+                    //parses the entries to see if they are numeric
+                    weight = Double.parseDouble(txtWeight.getText());
+                    height = Double.parseDouble(txtHeight.getText());
+
+                    //Calculates the BMI with imperial units
+                    BMI = Math.round((weight*703)/(height*height)*100.0)/100.0;
+
+                    //output the BMI and clear the error label
                     outputBMI.setText(Double.toString(BMI));
+                    errorLbl.setText("");
                 }
                 catch (NumberFormatException e)
                 {
-                    System.out.println("Error: " + e + " Weight and height must be numeric.");
+                    //Outputs error message in error label and clears inputs.
+                   errorLbl.setText("Error: Weight and height must be numeric.");
+                   txtHeight.setText("");
+                   txtWeight.setText("");
+                   outputBMI.setText("");
                 }
 
 
@@ -66,6 +86,7 @@ public class Main extends Application
         }
         calculate.setOnAction(new ButtonClickerHandler());
 
+        //HBOX DECLARATIONS
         HBox hbox =new HBox(10,bmiTableView);
         HBox heightLblBox = new HBox(10,MyHeight);
         HBox weightLblBox = new HBox(10,MyWeight);
@@ -74,7 +95,11 @@ public class Main extends Application
         HBox weightEntry = new HBox(10,txtWeight);
         HBox outputBox = new HBox(10,lblBMI);
         HBox BMIBox = new HBox(10,outputBMI);
+        HBox errorBox = new HBox(10,errorLbl);
+
         BMIBox.setStyle("-fx-border-color: #000000; -fx-alignment: center; ");
+
+        //Populates GridPane with the HBox objects
         GridPane gridPane = new GridPane();
         gridPane.setVgap(10);
         gridPane.setHgap(10);
@@ -87,7 +112,10 @@ public class Main extends Application
         gridPane.add(buttonBox,0,4);
         gridPane.add(outputBox,0,3);
         gridPane.add(BMIBox,1,3);
-        gridPane.setStyle("-fx-font-size: 20px; -fx-font-family: 'Consolas'; ");
+        gridPane.add(errorBox,0,5,3,1);
+        //formats the grid pane to set the font size to 20
+        gridPane.setStyle("-fx-font-size: 20px; -fx-font-family: 'Consolas'; -fx-cell-size: 60px;");
+
         Scene scene = new Scene(gridPane, 640, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
